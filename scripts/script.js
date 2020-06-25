57ee26dce66a49f0e91b0d89b7588d7936de20be
Marina Pisareva <aniram1s@yandex.ru>
2020-06-26 00:52:10 +0300

    описала логику открытия/закрытия двух попапов

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,80 @@
-let popup = document.querySelector('.popup');
+let popupEditProfile = document.querySelector('.popup_type_edit-profile');
+let popupNewCard = document.querySelector('.popup_type_new-card');
 let editProfileButton = document.querySelector('.profile__edit-button');
-let closePopupButton = popup.querySelector('.popup__exit-button');
-let saveProfileButton = popup.querySelector('.popup__submit-button_type_save');
+let closePopupEditProfile = popupEditProfile.querySelector('.popup__exit-button_type_edit-profile');
+let closePopupNewCard = popupNewCard.querySelector('.popup__exit-button_type_new-card');
+let saveProfileButton = popupEditProfile.querySelector('.popup__submit-button_type_save');
 let nameInput = document.querySelector('.popup__input_type_name')
 let jobInput = document.querySelector('.popup__input_type_info')
 let profileName = document.querySelector('.profile__title');
 let profileJob = document.querySelector('.profile__subtitle'); 
-let formElement = popup.querySelector('.popup__form_type_edit-profile')
+let formElement = popupEditProfile.querySelector('.popup__form_type_edit-profile');
+let createNewCardButton = document.querySelector('.profile__add-button');
+
+
+function popupToggle(popup) {
+    popup.classList.toggle('popup_opened'); 
+}
+
+function formSubmitHandler (evt) {               // эта функция вставляет в поля на странице данные из формы, которые ввел
+    evt.preventDefault();                        // пользователь и закрывает попап после подтверждния
+
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+
+    popupToggle(popupEditProfile);
+} 
+
+function overlayClick (event) {                   //эта функция закрывает попап при клике на пространство вокруг него
+    if (event.target == event.currentTarget) {
+        popupToggle ()
+    }
+
+}
+
+
+closePopupEditProfile.addEventListener('click', function (){        //обработчик на закрывающий попап крестик 
+    popupToggle(popupEditProfile);
+});   
+
+editProfileButton.addEventListener('click', function (){      // обработчик на кнопку редактирования профиля, которая открывает попап
+    popupToggle(popupEditProfile);
+    if (popupEditProfile.classList.contains('popup_opened')) {
+        nameInput.value = profileName.textContent;
+        jobInput.value = profileJob.textContent;
+
+    }
+});  
+
+formElement.addEventListener('submit', formSubmitHandler);  // обработчик на саму форму
+
+//!!      popupEditProfile.addEventListener('click', overlayClick);   //ПОДУМАТЬ КАК РЕШИТЬ ВОПРОС обработчик для пространства вокруг попапа, закрывающий его при клике
+
+createNewCardButton.addEventListener('click', function() {
+    popupToggle(popupNewCard);
+})
+closePopupNewCard.addEventListener('click', function() {
+    popupToggle(popupNewCard);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*function openPopup() {                           //добавляем класс, который открывает попап
     popup.classList.add('popup_opened');
@@ -19,39 +87,3 @@ function closePopup() {                          //снимаем класс с 
     popup.classList.remove('popup_opened');
 }
 */
-
-function popupToggle() {
-    popup.classList.toggle('popup_opened'); 
-}
-
-function formSubmitHandler (evt) {               // эта функция вставляет в поля на странице данные из формы, которые ввел
-    evt.preventDefault();                        // пользователь и закрывает попап после подтверждния
-
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
-
-    popupToggle()
-} 
-
-function overlayClick (event) {                   //эта функция закрывает попап при клике на пространство вокруг него
-    if (event.target == event.currentTarget) {
-        popupToggle ()
-    }
-
-}
-
-
-closePopupButton.addEventListener('click', popupToggle);     //обработчик на закрывающий попап крестик 
-editProfileButton.addEventListener('click', function (){
-    popupToggle();
-    if (popup.classList.contains('popup_opened')) {
-        nameInput.value = profileName.textContent;
-        jobInput.value = profileJob.textContent;
-
-    }
-});     // обработчик на кнопку редактирования профиля, которая открывает попап
-formElement.addEventListener('submit', formSubmitHandler);  // обработчик на саму форму
-popup.addEventListener('click', overlayClick);              //обработчик для пространства вокруг попапа, закрывающий его при клике
-
-
-
