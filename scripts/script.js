ba2c2689fd18c665b785c254d618185ecab03a35
Marina Pisareva <aniram1s@yandex.ru>
2020-06-26 23:26:50 +0300

    cards add complete

--- scripts/script.js
+++ scripts/script.js
@@ -47,7 +47,47 @@ closePopupNewCard.addEventListener('click', function() {
     popupToggle(popupNewCard);
 });
 
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
 
+const templateCards = document.querySelector('.template-cards');
+const cardsList = document.querySelector('.grid-elements')
+
+function addCards (item) {
+    const card = templateCards.content.cloneNode(true);
+    card.querySelector('.grid-element__title').textContent = item.name;
+    card.querySelector('.grid-element__image').src = item.link;
+
+    cardsList.prepend(card)
+} 
+
+initialCards.forEach(item => {
+   addCards(item);
+});
 
 
 
